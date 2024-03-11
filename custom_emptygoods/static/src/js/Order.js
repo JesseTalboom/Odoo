13758e54b915d35456cc92ce3863957ad31ddbb1
JesseTalboom <107983761+JesseTalboom@users.noreply.github.com>
2024-03-11 10:33:02 +0100

    POS unmerge emptygoods and link between empty and full goods

--- custom_emptygoods/static/src/js/Order.js
+++ custom_emptygoods/static/src/js/Order.js
@@ -6,28 +6,64 @@ odoo.define('custom_emptygoods.Order', function(require) {
 
      const OrderEmptyGoods = (Order) => class OrderEmptyGoods extends Order {
 
-         // Inherits add_product method, if product has an emptygoods product linked this will add it as new line, if this line is already existing quantity is updated
+         // Inherits add_product method, if product is fullgoods add the emtpygoods as seperate line
          async add_product(product, options) {
-             var res = super.add_product(product, options);
+             // Check if product already exists, if so emptygoods line added can be merged, if not do not merge emptygoods
+             const lines = this.get_orderlines();
+             const productExists = lines.filter(x => x.product.id == product.id)[0] ?? null;
 
-             var emptygoodsProduct = this.pos.db.get_product_by_id(product.emptygoods_product_id[0])
-             if (emptygoodsProduct){
-                 const lines = this.get_orderlines();
-                 const emptyGoodLine = lines.filter(x => x.product == emptygoodsProduct)[0] ?? null;
-                 // Already existing - Update
-                 if (emptyGoodLine)
-                 {
-                     const oldQuantity = emptyGoodLine.quantity;
-                     const newQuantity = oldQuantity + 1;
-                     emptyGoodLine.set_quantity(newQuantity)
-                 }
-                 // Non-existing - Create
-                 else
-                 {
-                    super.add_product(emptygoodsProduct)
-                 }
+             // If an emptygoods product is added, this may never be merged
+
+             if (product.emptygoods){
+                 options.merge = false;
              }
 
+             // SUPER
+             var res = super.add_product(product, options);
+
+             // Find emptygoods product
+             var emptygoodsProduct = this.pos.db.get_product_by_id(product.emptygoods_product_id[0])
+             if (emptygoodsProduct){
+                 // MERGE *OLD CODE*
+                 // const lines = this.get_orderlines();
+                 // const emptyGoodLine = lines.filter(x => x.product == emptygoodsProduct)[0] ?? null;
+                 // // Already existing - Update
+                 // if (emptyGoodLine)
+                 // {
+                 //     const oldQuantity = emptyGoodLine.quantity;
+                 //     const newQuantity = oldQuantity + 1;
+                 //     emptyGoodLine.set_quantity(newQuantity)
+                 // }
+                 // // Non-existing - Create
+                 // else
+                 // {
+                 //    super.add_product(emptygoodsProduct)
+                 // }
+
+                 let opt = {};
+
+                 const fullgoods_line = super.get_selected_orderline();
+                 opt.fullgoods_line_id = fullgoods_line.id;
+                 opt.description = fullgoods_line.product.display_name;
+
+                 if (!productExists){
+                     opt.merge = false;
+                 }
+
+                 super.add_product(emptygoodsProduct, opt)
+
+                 const emptygoods_line = super.get_selected_orderline();
+                 fullgoods_line.emptygoods_line_id = emptygoods_line.id;
+             }
+
+             return res;
+         }
+
+         set_orderline_options(orderline, options) {
+             var res = super.set_orderline_options(orderline, options);
+             if (options.fullgoods_line_id){
+                orderline.fullgoods_line_id = options.fullgoods_line_id;
+             }
              return res;
          }
      }
--- custom_emptygoods/static/src/js/OrderLine.js
+++ custom_emptygoods/static/src/js/OrderLine.js
@@ -6,26 +6,32 @@ odoo.define('custom_emptygoods.Orderline', function(require) {
 
      const OrderlineEmptyGoods = (Orderline) => class OrderlineEmptyGoods extends Orderline {
 
+         // Update quantity of empty goods line
          set_quantity(quantity, keep_price) {
              const oldQuantity = this.quantity ?? 0;
              // Check if oldQuantity is not 0, because if 0 then this method is called from constructor and empty good may not be added since this is already added from Order.add_product.
              if (oldQuantity > 0)
              {
-                 const quantityDiff = quantity - oldQuantity;
-
-                 const emptygoodProduct = this.pos.db.get_product_by_id(this.product.emptygoods_product_id[0])
-                 if (emptygoodProduct)
-                 {
-                     const lines = this.order.get_orderlines();
-                     const emptyGoodLine = lines.filter(x => x.product == emptygoodProduct)[0] ?? null;
-                     if (emptyGoodLine)
-                     {
-                         //const newQuantity = Math.max(emptyGoodLine.quantity + quantityDiff, 0);
-                         const oldQuantity = emptyGoodLine.quantity;
-                         const newQuantity = oldQuantity + quantityDiff;
-                         emptyGoodLine.set_quantity(newQuantity)
-                     }
+                 // If fullgoods line is edited, update emptygoods line aswell
+                 if (this.emptygoods_line_id){
+                     const emptygoods_line = this.order.get_orderline(this.emptygoods_line_id);
+                     emptygoods_line.set_quantity(quantity);
                  }
+
+                 // const quantityDiff = quantity - oldQuantity;
+                 // const emptygoodProduct = this.pos.db.get_product_by_id(this.product.emptygoods_product_id[0])
+                 // if (emptygoodProduct)
+                 // {
+                 //     const lines = this.order.get_orderlines();
+                 //     const emptyGoodLine = lines.filter(x => x.product == emptygoodProduct)[0] ?? null;
+                 //     if (emptyGoodLine)
+                 //     {
+                 //         //const newQuantity = Math.max(emptyGoodLine.quantity + quantityDiff, 0);
+                 //         const oldQuantity = emptyGoodLine.quantity;
+                 //         const newQuantity = oldQuantity + quantityDiff;
+                 //         emptyGoodLine.set_quantity(newQuantity)
+                 //     }
+                 // }
              }
 
              return super.set_quantity(quantity, keep_price);
